072cf513eafc88e6b5372fe3d20864a0616c00d9
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 22:40:50 +0530

    Adding EightObject

---
+++ core/src/com/mygdx/game/objects/EightObject.java
@@ -0,0 +1,35 @@
+package com.mygdx.game.objects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by anweshmishra on 06/03/17.
+ */
+public class EightObject {
+    private float x,y,w,dir = 0,deg=0;
+    public EightObject(float x,float y,float w) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+    }
+    public void draw(Pixmap pixmap,int w,int h) {
+        pixmap.fill();
+        pixmap.setColor(Color.ORANGE);
+        pixmap.drawCircle(w/2,h/4,w/2);
+        pixmap.drawCircle(w/2,h/2+h/4,w/2);
+    }
+    private void update() {
+        x+=dir*5;
+        deg+=dir*5;
+    }
+    public void render(Sprite sprite, SpriteBatch batch) {
+        update();
+        sprite.setPosition(x,y);
+        sprite.setRotation(deg);
+        sprite.draw(batch);
+    }
+
+}
