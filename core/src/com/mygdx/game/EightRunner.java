dd5fc864af378baedaba169771005b2a46b41e93
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 21:13:01 +0530

    Adding EightRunner Application

---
+++ core/src/com/mygdx/game/EightRunner.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/**
+ * Created by anweshmishra on 06/03/17.
+ */
+public class EightRunner extends ApplicationAdapter{
+    public void render() {
+
+    }
+    public void create() {
+
+    }
+    public void dispose() {
+
+    }
+}
