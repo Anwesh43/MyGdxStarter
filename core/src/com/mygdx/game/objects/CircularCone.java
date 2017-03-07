6a7b3f440172eabd3626cf59a88363bbdb312b45
Anwesh43 <anweshthecool0@gmail.com>
2017-03-07 16:27:37 +0530

    Adding CircularCone class

---
+++ core/src/com/mygdx/game/objects/CircularCone.java
@@ -0,0 +1,38 @@
+package com.mygdx.game.objects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by anweshmishra on 07/03/17.
+ */
+public class CircularCone {
+    private float x,y,deg=0,w = 100,h = 100,dir = -1;
+    public CircularCone(float w,float h) {
+        this.w = w;
+        this.x = 0;
+        this.y = h/2;
+    }
+    public void draw(Pixmap pixmap) {
+        pixmap.setColor(Color.ORANGE);
+        pixmap.fillCircle(pixmap.getWidth()/2,pixmap.getHeight()/2,pixmap.getWidth()/4);
+        pixmap.fillTriangle(pixmap.getWidth()/2,pixmap.getHeight()/2-pixmap.getWidth()/4,0,0,pixmap.getWidth(),0);
+    }
+    private void update() {
+        x+=dir*10;
+        if(Math.abs(deg)<90) {
+            deg+=10*dir;
+        }
+    }
+    public void setDir(float dir) {
+        this.dir = dir;
+        deg+=10*dir;
+    }
+    public void render(Sprite sprite, SpriteBatch spriteBatch) {
+        sprite.setPosition(x,y);
+        sprite.setRotation(deg);
+        sprite.draw(spriteBatch);
+    }
+}
