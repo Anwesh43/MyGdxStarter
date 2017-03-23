5981028705ebbfe6c595540c31508da819c57d06
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 15:53:15 +0530

    Adding animationqueue

--- core/src/com/mygdx/game/AnimationQueue.java
+++ core/src/com/mygdx/game/AnimationQueue.java
@@ -1,7 +1,23 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anweshmishra on 23/03/17.
  */
 public class AnimationQueue {
+    private List<ObjectAnimation> animations = new ArrayList<ObjectAnimation>();
+    public void addAnimation(ObjectAnimation objectAnimation) {
+        animations.add(objectAnimation);
+    }
+    public void execute() {
+        while(animations.size()>0) {
+            ObjectAnimation objectAnimation = animations.get(0);
+            objectAnimation.animate();
+            if(objectAnimation.isDone()) {
+                animations.remove(objectAnimation);
+            }
+        }
+    }
 }
